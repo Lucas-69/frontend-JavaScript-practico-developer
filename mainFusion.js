5b13a5384f14be8b327c2e233abbc2383b84cfe4
Lucas Rios <rioslucas97@gmail.com>
2023-08-05 15:08:53 -0300

    agregando mas funcionalidades al sitio

--- mainFusion.js
+++ mainFusion.js
@@ -1,8 +1,10 @@
 //seleccionamos el elemento y lo guardamos en una variable
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+
 const menuCarritoImg = document.querySelector('.navbar-shopping-cart');
 const menuCarrito = document.querySelector('.product-detail');
 
@@ -10,9 +12,16 @@ const menuCarrito = document.querySelector('.product-detail');
 menuEmail.addEventListener('click', toggleDesktopMenu);
 /* si hago click en la img del menu hamburguesa va a mostrarme o no el menu mobil del navbar */
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 menuCarritoImg.addEventListener('click', toggleBtnCarriImg);
 
 function toggleDesktopMenu(){
+    const isMenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    
+    if(!isMenuCarritoClosed){
+        menuCarrito.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive'); //creamos la funcion classList.toggle que quita o pone la opcion inactive dependiendo si esta o no..
 }
 function toggleMobileMenu(){
@@ -25,13 +34,19 @@ function toggleMobileMenu(){
     }
     mobileMenu.classList.toggle('inactive');
 }
-
 function toggleBtnCarriImg(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    //cerramos el menu desktop cuando abrimos el carrito 
+    const isDesktopClosed = desktopMenu.classList.contains('inactive');
 
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
-    
+
+    //preguntamos si esta abierto el menu desktop y si es asi lo cerramos
+    if(!isDesktopClosed){
+        desktopMenu.classList.add('inactive');
+    }
     menuCarrito.classList.toggle('inactive');
 }
