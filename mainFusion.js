595446a8618a65b820f8cad3104f396079f7ff5e
Lucas Rios <rioslucas97@gmail.com>
2023-08-05 17:40:20 -0300

    agregando Productos con JS

--- mainFusion.js
+++ mainFusion.js
@@ -8,6 +8,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoImg = document.querySelector('.navbar-shopping-cart');
 const menuCarrito = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 //creamos un escuchador de eventos y un nombre de funcion toggleDesktopMenu que aun no creamos
 menuEmail.addEventListener('click', toggleDesktopMenu);
 /* si hago click en la img del menu hamburguesa va a mostrarme o no el menu mobil del navbar */
@@ -50,3 +52,61 @@ function toggleBtnCarriImg(){
     }
     menuCarrito.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Compu',
+    precio: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: 'Bici',
+    precio: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: 'Celulares',
+    precio: 1120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProductos(arr){
+    for (product of arr){
+    const productCart = document.createElement('div');
+    productCart.classList.add('product-card');
+
+    //product = {name, precio, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productDivSolo = document.createElement('div');
+    
+    const productPrecio = document.createElement('p');
+    productPrecio.innerText = '$' + product.precio;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    
+    productDivSolo.appendChild(productPrecio);
+    productDivSolo.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    //vamos a meter nuestra imagen dentro de Figure 
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productDivSolo);
+    productInfo.appendChild(productInfoFigure);
+
+    productCart.appendChild(productImg);
+    productCart.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCart);
+    }
+}
+
+renderProductos(productList);
